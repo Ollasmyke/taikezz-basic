673157af2a6ccbe09b6a9e357400f547f68c1f3b
tobidosumu <dosunmumishael@gmail.com>
2023-04-21 20:37:01 +0100

    add footer

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function footer() {
   return (
-    <footer className="w-full h-[400px] bg-secondary">
+    <footer className="w-full h-[250px] bg-secondary">
       
     </footer>
   )
