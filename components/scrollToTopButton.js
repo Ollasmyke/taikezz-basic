9795079b9202dc605738f6de41910de0ff53a14b
tobidosumu <dosunmumishael@gmail.com>
2023-04-24 13:46:27 +0100

    add scrollToTop button

---
+++ components/scrollToTopButton.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
+import cn from 'classnames';
+
+// import KeyboardArrowUpRoundedIcon from '@mui/icons-material/KeyboardArrowUpRounded';
+import ArrowUpIosRoundedIcon from '@mui/icons-material/ArrowBackIosRounded';
+
+
+const ScrollToTopButton = ({ isVisible }) => {
+  const handleScrollTop = () => {
+    scroll.scrollToTop({ smooth: true });
+  };
+
+  return (
+    <div
+      className={cn(
+        'fixed bottom-4 right-10 z-50',
+        isVisible ? 'opacity-100' : 'opacity-0'
+      )}
+    >
+      <ScrollLink to="top" smooth duration={10}>
+        <button
+          className="scrollToTopBtn"
+          onClick={handleScrollTop}
+        >
+          <ArrowUpIosRoundedIcon />
+        </button>
+      </ScrollLink>
+    </div>
+  );
+};
+
+export default ScrollToTopButton;
