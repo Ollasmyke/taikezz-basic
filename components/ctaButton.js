56cc081fe4fe352025388d737ef9bb368661908a
tobidosumu <dosunmumishael@gmail.com>
2023-04-24 22:24:48 +0100

    change background color

--- components/ctaButton.js
+++ components/ctaButton.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const CtaButton = ({ link, text }) => {
   return (
     <Link href={link} className="float-right">
-      <button className="text-[12px] px-16 py-3 text-myWhite bg-secondary hover:bg-blue-900">
+      <button className="ctaBtn">
         {text}
       </button>
     </Link>
