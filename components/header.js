83a4fd9ee2915d8478909f4830f196546f11a9fc
tobidosumu <dosunmumishael@gmail.com>
2023-04-24 22:24:08 +0100

    add drop shadow on scroll to header

--- components/header.js
+++ components/header.js
@@ -1,41 +1,66 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
 import monoColorLogo from '../public/logos/taikezMonoColorLogo.svg';
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+useEffect(() => {
+  function handleScroll() {
+    if (window.scrollY > 0) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  }
+
+  window.addEventListener('scroll', handleScroll);
+  return () => {
+    window.removeEventListener('scroll', handleScroll);
+  };
+}, []);
+
+const headerStyle = isScrolled
+  ? {
+      filter: 'drop-shadow(0px 2.5px 2px rgb(0 0 0 / .06))',
+    }
+  : {};
   return (
-    <header className="sticky top-0 z-50 w-full flex item justify-between h-20 px-16 border-b bg-white">
+    <header 
+      style={headerStyle}
+      className="sticky top-0 z-50 w-full h-24 flex justify-between px-16 border-b bg-white">
       {/* Logo section */}
-      <div className="w-[33%] h-full">
-        <Link href="/" className="flex justify-between items-end">
+      <div className="w-[28%] h-full">
+        <Link href="/" className="mt-4 flex">
           <Image
             src={monoColorLogo}
             alt="Taikez logo"
-            className="w-[120px] h-[120px] -mb-[50px] rounded-full bg-white"
+            className="w-[120px] h-[120px] p-2 rounded-full bg-white"
           />
-          <h2 className="text-[21px] font-medium text-myBlack">Taikez Consultancy</h2>
+          <h2 className="font-jost text-[26px] absolute bottom-0 left-48 font-medium text-secondary">Taikez Consultancy</h2>
         </Link>
       </div>
 
       {/* Menu section */}
-      <div className="flex items-center h-full">
+      <div className="flex w-[65%] justify-end items-end pb-1 h-full">
         <nav>
-          <ul className="font-jost flex space-x-10 text-[16px] text-myBlack font-normal">
-            <li>
-              <Link href="/pages/about">About Us</Link>
+          <ul className="w-[100%] space-x-12 font-jost flex justify-between text-[16px] text-myBlack font-normal">
+            <li className="headerLi">
+              <Link href="/about">About Us</Link>
             </li>
-            <li>
+            <li className="headerLi">
               <Link href="/pages/services">Services</Link>
             </li>
-            <li>
+            <li className="headerLi">
               <Link href="/pages/press">Press</Link>
             </li>
-            <li>
+            <li className="headerLi">
               <Link href="/pages/team">Team</Link>
             </li>
-            <li>
+            <li className="headerLi">
               <Link href="/pages/contact">Contact</Link>
             </li>
           </ul>
