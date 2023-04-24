23db73cbb9c1543ac67d938f55b1a6e27297d486
tobidosumu <dosunmumishael@gmail.com>
2023-04-24 16:47:17 +0100

    add caption for each slide

--- components/carouselSlider.js
+++ components/carouselSlider.js
@@ -1,15 +1,38 @@
-import { useState, useRef, useEffect } from "react";
-import Image from "next/image";
-import cn from "classnames";
+import { useState, useRef, useEffect } from 'react';
+import Image from 'next/image';
+import cn from 'classnames';
 
 import ArrowBackIosRoundedIcon from '@mui/icons-material/ArrowBackIosRounded';
 import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 
 const images = [
-  { src: "/../public/images/slideOne.jpg", alt: "Image 1" },
-  { src: "/../public/images/slideTwo.jpg", alt: "Image 1" },
-  { src: "/../public/images/slideThree.jpg", alt: "Image 1" },
-  { src: "/../public/images/slideFour.jpg", alt: "Image 1" },
+  {
+    src: '/../public/images/slideOne.jpg',
+    alt: 'Reliable consultancy for quality site projects & modern construction',
+    description: 'Experience reliable and efficient construction services with our team of expert engineers and quality assurance professionals. We deliver quality projects on time with safety adherence.',
+    link: 'https://example.com/image1',
+  },
+  {
+    src: '/../public/images/slideTwo.jpg',
+    alt: "Streamline Your Organization's Procurement Process with our expertise",
+    description:
+      'Our procurement team specializes in sourcing and purchasing general merchandise and equipment while negotiating the best prices and contracts for organizations.',
+    link: 'https://example.com/image2',
+  },
+  {
+    src: '/../public/images/slideThree.jpg',
+    alt: "Efficient and Reliable Labour Sourcing for Your Organization's Needs",
+    description:
+      'Learn how our consultancy company outsources skilled and unskilled labour, creating a reliable and cost-effective supply chain network. Discover our recommended steps.',
+    link: 'https://example.com/image3',
+  },
+  {
+    src: '/../public/images/slideFour.jpg',
+    alt: "Unlock Your Business Potential: Let Us Help You Grow Beyond Expectations",
+    description:
+      'At our consulting firm, we help businesses thrive by providing expert advice on growth and establishment strategies, backed by market research and a team of expatriates.',
+    link: 'https://example.com/image4',
+  },
 ];
 
 const CarouselSlider = () => {
@@ -23,7 +46,7 @@ const CarouselSlider = () => {
       if (!isPaused) {
         setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
       }
-    }, 3000);
+    }, 5000);
 
     // Clear the interval when the component is unmounted or the active slide is changed
     return () => clearInterval(intervalRef.current);
@@ -35,11 +58,15 @@ const CarouselSlider = () => {
   };
 
   const handlePrevClick = () => {
-    setActiveIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+    setActiveIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
   };
 
   const handleNextClick = () => {
-    setActiveIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    setActiveIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   const handleMouseEnter = () => {
@@ -60,23 +87,42 @@ const CarouselSlider = () => {
             alt={image.alt}
             layout="fill"
             objectFit="cover"
-            className={cn("absolute top-0 left-0 w-full h-full transition-opacity", {
-              "opacity-100": activeIndex === index,
-              "opacity-0": activeIndex !== index,
-            })}
+            className={cn(
+              'absolute top-0 left-0 w-full h-full transition-opacity',
+              {
+                'opacity-100': activeIndex === index,
+                'opacity-0': activeIndex !== index,
+              }
+            )}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
           />
         ))}
       </div>
+      <div className="font-jost w-[40%] px-4 pt-3 pb-7 absolute bottom-10 bg-white/[0.7] left-1/2 transform -translate-x-1/2 text-center"
+                  onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+      >
+        <h3 className="text-xl text-myBlack leading-normal capitalize font-medium border-b pb-2 mb-3">{images[activeIndex].alt}</h3>
+        <p className="text-base text-gray-800 leading-tight mb-7">{images[activeIndex].description}</p>
+        <a
+          href={images[activeIndex].link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ctaBtn"
+        >
+          Learn More
+        </a>
+      </div>
+
       <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2">
         {images.map((image, index) => (
           <button
             key={index}
             onClick={() => handleIndicatorClick(index)}
-            className={cn("w-3 h-3 rounded-full mr-2 transition-colors", {
-              "bg-gray-500": activeIndex !== index,
-              "bg-white": activeIndex === index,
+            className={cn('w-2 h-2 rounded-full mr-4 transition-colors', {
+              'bg-secondary': activeIndex !== index,
+              'bg-white': activeIndex === index,
             })}
           />
         ))}
@@ -91,11 +137,11 @@ const CarouselSlider = () => {
       >
         <ArrowBackIosRoundedIcon />
       </button>
-      
+
       <button
         className="slideBtn right-24"
         style={{ opacity: isPaused ? 1 : 0 }}
-        onClick={handlePrevClick}
+        onClick={handleNextClick}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
--- components/layout.js
+++ components/layout.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import Head from 'next/head'
+import Head from 'next/head';
+import { useState, useEffect } from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 
 import Header from './header';
 import Footer from './footer';
+import ScrollToTopButton from '../components/scrollToTopButton';
 
 export default function Layout({ title, children }) {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = () => {
+    if (window.pageYOffset > 200) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <Head>
         <title>
-          {title
-            ? title + '- Taikez Consultancy Service'
-            : 'Taikez'}
+          {title ? title + '- Taikez Consultancy Service' : 'Taikez'}
         </title>
         <meta
           name="description"
@@ -22,8 +40,10 @@ export default function Layout({ title, children }) {
       </Head>
 
       <Header />
-      <main className="w-full">{ children }</main>
+      <main className="w-full">{children}</main>
       <Footer />
+
+      <ScrollToTopButton isVisible={isScrolled} />
     </>
   );
 }
