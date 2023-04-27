e0e670956f22a9af7551dbc52f05a70f369ee744
tobidosumu <dosunmumishael@gmail.com>
2023-04-27 13:29:45 +0100

    now mobile responsive

---
+++ pages/about.js
@@ -0,0 +1,27 @@
+import Layout from './../components/layout';
+
+import CarouselSlider from './../components/carouselSlider';
+import Offers from './../components/sections/offers';
+import ConstructionProjects from './../components/sections/constructionProjects';
+import SupplySourcing from '../components/sections/supplySourcing';
+import ClientFeedback from './../components/sections/clientFeedback';
+import Process from './../components/sections/process';
+import MeetOurTeam from './../components/sections/meetOurTeam';
+import Partners from './../components/sections/partners';
+import ContactUs from './../components/sections/contactUs';
+
+export default function sections() {
+  return (
+    <Layout title="About ">
+      <CarouselSlider />
+      <Offers />
+      <ConstructionProjects />
+      <SupplySourcing />
+      <ClientFeedback />
+      <Process />
+      <MeetOurTeam />
+      <Partners />
+      <ContactUs />
+    </Layout>
+  );
+}
