d5da646ad6f69765fe93fb44ef9153353a558ff9
tobidosumu <dosunmumishael@gmail.com>
2023-04-27 15:25:25 +0100

    footer is now mobile responsive

--- components/footer.js
+++ components/footer.js
@@ -1,24 +1,55 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
-import WhiteLogo from './../public/logos/whiteTaikezLogo.svg'
+import CallRoundedIcon from '@mui/icons-material/CallRounded';
+import MailRoundedIcon from '@mui/icons-material/MailRounded';
+
+import WhiteLogo from './../public/logos/whiteTaikezLogo.svg';
 
 export default function Footer() {
   return (
-    <footer className="w-full h-[330px] px-[160px] py-20 flex justify-between text-myWhite bg-secondary">
-      <div className="w-[30%]">
-        <Image src={WhiteLogo} alt="Taikez logo" className="w-[120px] h-[120px]" />
+    <footer className="sectionDefault lg:flex lg:justify-between space-y-14 lg:space-y-0 text-myWhite bg-secondary">
+      <div className="lg:w-[40%] flex items-center space-x-3.5">
+        <Image
+          src={WhiteLogo}
+          alt="Taikez logo"
+          className="w-[120px] h-[120px]"
+        />
+        <div className="font-jost">
+          <h2 className="text-[12px] md:text-[20px] font-normal text-myWhite">
+            Taikez Consultancy Service
+          </h2>
+          <p className="text-[14px] font-light -mt-1 italic">Our smile, your maximum satisfaction</p>
+          <p className="text-[14px] font-light mt-1">REGISTRATION NUMBER: 1875384</p>
+        </div>
       </div>
-      <div className="w-[30%] space-y-[2px]">
-        <h3 className="text-base uppercase font-medium">CONTACTS</h3>
-        <p className="paragraph myDarkGray">123-456-7890</p>
-        <p className="paragraph myDarkGray">info@presson.com</p>
+      <div className="lg:w-[25%] flex flex-col space-y-[2px] text-myWhite">
+        <h3 className="text-base uppercase font-medium mb-2">CONTACTS</h3>
+        <Link className="paragraph w-fit" href="tel:09019077479">
+          <CallRoundedIcon className="w-4 mr-2" />
+          0901 907 7479
+        </Link>
+        <Link className="paragraph w-fit" href="tel:08038567604">
+          <CallRoundedIcon className="w-4 mr-2" />
+          0803 856 7604
+        </Link>
+        <Link className="paragraph w-fit" href="mailto:taikez@demdarzzy.com">
+          <MailRoundedIcon className="w-4 mr-2" />
+          taikez@demdarzzy.com
+        </Link>
       </div>
-      <div className="w-[30%] space-y-[2px]">
-        <h3 className="text-base uppercase font-medium">ADDRESS</h3>
-        <p className="paragraph myDarkGray">123 your street name,</p>
-        <p className="paragraph myDarkGray">City name, Country, post code </p>
+      <div className="lg:w-[25%] space-y-[2px]">
+        <h3 className="text-base uppercase font-medium mb-2">ADDRESS</h3>
+        <p className="paragraph">
+          <span className="font-normal">
+            Head Office:
+            </span> 
+          21, Togedejoye Street,
+          <br /> Ojokoro Village, Agric Ikorodu,{' '}
+        </p>
+        <p className="paragraph"> Lagos State, Nigeria, 102222 </p>
       </div>
     </footer>
-  )
+  );
 }
